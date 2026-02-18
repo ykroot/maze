8f6bd6b0fe3f351d8523a82a3150602a31e5d84e
Mouncif Aguilounat <mouaguil@e2r4p5.1337.ma>
2026-02-18 19:23:59 +0000

    2

--- src/read_parse.py
+++ src/read_parse.py
@@ -29,7 +29,7 @@ def read_config(file_name): str) -> None:
 
 def parse_coords(coords: dict = {}):
 
-    if "ENTRY" in coords and "EXIT" in coords and "PERFECT":
+    if "ENTRY" in coords and "EXIT" in coords and "PERFECT" in coords:
         ee_values = []
         ee_values.append(coords("ENTRY"))
         ee_values.append(coords("EXIT"))
@@ -38,53 +38,52 @@ def parse_coords(coords: dict = {}):
         print("Error: the config file misses one or more elements!")
         return
 
-    coords.pop("ENTRY", None)
-    coords.pop("EXIT", None)
-    coords.pop("PERFECT", None)
     try:
         values = []
         for key, value in coords.items():
+            if key == "ENTRY" or key == "EXIT" or key == "PERFECT":
+                continue
             values.append(int(value))
 
         if not isinstance(perfect_value, bool):
-            raise ValueError(
+            raising(
             f"Error: {last_key} element has an invalid Value ({last_value})"
             )
 
         for value in values:
             if value < 0:
-                raise ValueError(
-                        f"Error: {coords.keys{value)} can not be negative!"
-                        )
+                raising(
+                        f"Error: {coords.keys(value)} can not be negative!"
+                    )
 
-    ee_items = []
     for value in ee_values:
 
         parts = value.split(',')
 
         if value.count(',') != 1 or len(parts) != 2:
-            raise ValueError("Error: the item {coords.key(value)} is invalid")
+            raising("Error: the item {coords.key(value)} is invalid")
 
-        x, y = int(parts[0]), int(parts[1])
-        ee_items.append((x, y))
-        """
-        still needs a check if the width and length are inside the maze , (see GAS)
-        and also check if the ee values are not None
-        """
+        x, y = int(parts[0]), int(parts[1]):
+        coords[ #i need to make sure that the values of ee's are converted to int and are a tuples
+ 
+        validation(coords)
     except ValueError as e:
-        if "invalid literal" in str(e):
-            print(f"Error: an error happend while parsing")
-        else:
-            print(e)
+        print(e)
         return
     except Exception as e:
         print(e)
+        return
 
 
+def validation(coords: dict):
+    """
+    still needs a check if the width and length are inside the maze , (see GAS)
+    and also check if the ee values are not None
+    """ 
 
-
-
-
+def raising(err_msg: str):
+    raise ValueError(message)
+    exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
